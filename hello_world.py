9432d7adda9213a97bc84088da08135decbe6e1c
Jigna Patel <jignap@gmail.com>
2019-04-08 23:02:31 -0400

    fix merge conflict

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,6 @@
 a = [1, 2, 3, 4]
 for item in a:
     if item > 1:
-<<<<<<< HEAD
-        print("this is  one")
-=======
         print("this is jigna changed this that one")
->>>>>>> ba3590c17b24a60813834321bdb98f6225545193
     else:
         print("this is less than or equal to 1")
